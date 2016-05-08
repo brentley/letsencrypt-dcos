dd6bb67c9d60d649ca924a475ce6ef3fcd0dc3aa
Brent Langston <blangston@cloudpassage.com>
2016-05-08 13:10:38 -0400

    version hash

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 set -u
 
 name="letsencrypt-dcos"
-version="${version:-latest}"
+version=$(git rev-parse HEAD 2>/dev/null | cut -c 1-7)
 image="${image:-brentley/letsencrypt-dcos}"
 
 docker build -t $image:$version .
